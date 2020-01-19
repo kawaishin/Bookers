15237b1bf9d90fe11a3f48b7f8c26402208e2df0
kawaishin <believeshin0410@gmail.com>
2020-01-19 09:50:48 +0000

    database

--- db/schema.rb
+++ db/schema.rb
@@ -12,22 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20200113045644) do
 
-  create_table "book_comments", force: :cascade do |t|
-    t.text "comment"
-    t.integer "user_id"
-    t.integer "book_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "bookcomments", force: :cascade do |t|
-    t.text "comment"
-    t.integer "user_id"
-    t.integer "book_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "books", force: :cascade do |t|
     t.text "title"
     t.text "body"
